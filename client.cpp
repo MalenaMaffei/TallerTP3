df21e1d07cd1fd022d7e31600516939d1f6a83fd
MalenaMaffei <malenamaffei@hotmail.com>
2017-04-22 13:21:56 -0300

    vector de argumentos para el cliente

--- client.cpp
+++ client.cpp
@@ -1,23 +1,29 @@
 #include "common_Socket.h"
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 using std::string;
 using std::vector;
 
 #define CLIENT_MODE 1
 
-int client(const char *ip, const char *port, vector<string> params){
+int client(const char *ip, const char *port, vector<string> arguments){
     int status;
 
     Socket client_socket;
     status = client_socket.CreateAndConnect(ip, port);
     if (status < 0) { return 0; }
 
-//    string hola = "Hello world";
-    char *holaChar = &saludo[0];
-    client_socket.Send((unsigned char *)holaChar, saludo.length());
+    std::for_each(arguments.begin(), arguments.end(), [](string &a){
+            std::cout << "argumento: " << a << std::endl;
+    });
 
-    client_socket.Shutdown(CLIENT_MODE);
+//    string hola = "Hello world";
+//    char *holaChar = &saludo[0];
+//    client_socket.Send((unsigned char *)holaChar, saludo.length());
+//
+//    client_socket.Shutdown(CLIENT_MODE);
 //
 //    recv_and_print(&client_socket);
 //
@@ -30,8 +36,8 @@ int main(int argc, char **argv){
 //        char *server_ip = argv[2];
 //        char *server_port = argv[3];
 //        char *file_name = argv[4];
-    
-        client("127.0.0.1", "8080", );
+    std::vector<std::string> arguments(argv + 3, argv + argc);
+        client("127.0.0.1", "8080", arguments);
 
     return 0;
 }
