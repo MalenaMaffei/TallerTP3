9a876ecc9e634c8224ef07faa12aa3ceb59aba45
MalenaMaffei <malenamaffei@hotmail.com>
2017-04-22 19:23:39 -0300

    avances sesion

---
+++ server_Session.h
@@ -0,0 +1,16 @@
+#ifndef TP2_SESSION_H
+#define TP2_SESSION_H
+
+#include "common_Socket.h"
+#include "server_User.h"
+
+class Session {
+private:
+//    tambien deberia tener una fabrica que le crea los usuarios
+    Socket socket;
+    User user;
+
+};
+
+
+#endif //TP2_SESSION_H
